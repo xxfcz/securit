74ecefea0bf51df105a7bf1ea156e44d7f8b49eb
xxf_cz <xxf_cz@163.com>
2020-06-22 23:30:00 +0800

    添加项目文件。

---
+++ gp01/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace gp01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+/*            ComputerGroupPolicyObject.SetPolicySetting(@"HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate!WUServer", "http://10.160.5.20:8531", RegistryValueKind.String);
+            ComputerGroupPolicyObject.SetPolicySetting(@"HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate!WUStatusServer", "http://10.160.5.20:8531", RegistryValueKind.String);
+            ComputerGroupPolicyObject.SetPolicySetting(@"HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU!UseWUServer", "1", RegistryValueKind.DWord);
+*/
+            ComputerGroupPolicyObject.SetPolicySetting(@"HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer!NoDriveTypeAutoRun", "255", RegistryValueKind.DWord);
+           
+            // start gpedit.msc for the changes to take effects
+            Process p = Process.Start("gpedit.msc");
+            if(p.WaitForInputIdle())
+            {
+                try
+                {
+                    p.CloseMainWindow();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    p.Kill();
+                }
+            }
+        }
+    }
+}
